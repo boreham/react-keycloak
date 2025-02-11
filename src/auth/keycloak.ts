fd120380640a2abe7fa8d03d9353d58c81ae1fde
Kong Boreham <99660515+boreham@users.noreply.github.com>
2025-02-11 15:02:21 +0300

    Update keycloak.ts

--- src/auth/keycloak.ts
+++ src/auth/keycloak.ts
@@ -1,8 +1,8 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "https://192.168.8.149/",
-  realm: "elephant",
+  url: "https://localhost:8080/",
+  realm: "example",
   clientId: "public-client",
 });
 
